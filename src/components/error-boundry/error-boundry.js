47b4cc874fa41fac3c6d9f83c57666a8d8710f4b
Ivan Sergeev <ahrejlok2007@ya.ru>
2021-11-08 23:46:49 +0500

    add main react & redux components

---
+++ src/components/error-boundry/error-boundry.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import ErrorIndicator from "../error-indicator/error-indicator";
+
+export class ErrorBoundry extends Component {
+
+    state ={
+        hasError:false
+    }
+
+    componentDidCatch() {
+        this.setState({
+            hasError: true
+        })
+    }
+
+    render () {
+        const error = this.state.hasError;
+
+        if (error) {
+            return <ErrorIndicator />;
+        }
+
+        return this.props.children;
+    
+    }
+}
